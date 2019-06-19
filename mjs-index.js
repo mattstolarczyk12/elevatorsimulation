ee7d97f89f0ef2365f82a96b6cac4f53fcdaee74
mattstolarczyk12 <52009671+mattstolarczyk12@users.noreply.github.com>
2019-06-19 15:03:33 -0400

    Add files via upload

---
+++ mjs-index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// Node JS inheritance required for elevator simulation
+const program = require('commander');
+const {prompt} = require('inquirer');
+
+//elevator control implementation
+const Controller = require('./mjs-controller');
+
+// aka main for elevator control instance
+program
+    .version('1.0.0')
+    .option('-f --floors', 'Number of floors')
+    .option('-e --elevators', 'Number of elevators')
+    .option('-t --trips', 'Maximum number of trips before maintenance is needed')
+    .action(() => {
+        let controller = new Controller(
+            //pass inputs here, for example (3,2,100) for 3 floors, 2 elevators, and 100 max trips before maint
+        );
+    });
+
+function requestElevator(floor) {
+    // call the elevator from a floor
+}
+
+function sendElevator(index, floor) {
+    // once inside the elevator, send it to the desired floor
+}
